3ec3f7936096f556579ebdf58fd2f7d28732233d
Eduard <e.shengals@gmail.com>
2014-12-10 17:53:11 +0200

    viimane ver

--- main.py
+++ main.py
@@ -1,76 +1,73 @@
 __author__ = 'Meelis Tapo'
-__author__ = 'Eduard'
-
-#TODO game over graafika, pluss mängu uuesti laadimine pärast elude kaotust
-#TODO tondid loogika (chaser, ambusher, fickle, stupid)
-#TODO menüü taoline asjandus/hiscore/etc
-#TODO helid (kaustas olemas die.ogg, intro.ogg)
-#TODO powerupid
+__author__ = 'Eduard Šengals'
 
 from tkinter import *
 from game import *
 from objects import *
+from random import randint
 
 # akna loomine
-raam = Tk()
-raam.title("Pacman")
-raam.geometry("850x850")
-tahvel = Canvas(raam, width=850, height=850, bg="black")
-tahvel.grid_forget()
-menu = Canvas(raam, width=850, height=850, bg="black")
+frame = Tk()
+frame.title("Pacversity")
+frame.geometry("850x850")
+tahvel = Canvas(frame, width=850, height=850, bg="black")
+menu = Canvas(frame, width=850, height=850, bg="black")
 menu.grid()
-over = Canvas(raam, width=850, height=850, bg="black")
-over.grid_forget()
 
 # meedia sisselugemine
 immov_obj.wall_img = PhotoImage(file="media/wall.png")
 immov_obj.pellet_img = PhotoImage(file="media/pellet.png")
+immov_obj.menu_img = PhotoImage(file="media/menu.png")
 moving_obj.pac_imgs.append([PhotoImage(file="media/pacs/pac_e_1.png"),
-                            PhotoImage(file="media/pacs/pac_e_2.png"),
-                            PhotoImage(file="media/pacs/pac_e_3.png"),
-                            PhotoImage(file="media/pacs/pac_e_4.png")])
+                            PhotoImage(file="media/pacs/pac_e_2.png")])
 moving_obj.pac_imgs.append([PhotoImage(file="media/pacs/pac_n_1.png"),
-                            PhotoImage(file="media/pacs/pac_n_2.png"),
-                            PhotoImage(file="media/pacs/pac_n_3.png"),
-                            PhotoImage(file="media/pacs/pac_n_4.png")])
+                            PhotoImage(file="media/pacs/pac_n_2.png")])
 moving_obj.pac_imgs.append([PhotoImage(file="media/pacs/pac_s_1.png"),
-                            PhotoImage(file="media/pacs/pac_s_2.png"),
-                            PhotoImage(file="media/pacs/pac_s_3.png"),
-                            PhotoImage(file="media/pacs/pac_s_4.png")])
+                            PhotoImage(file="media/pacs/pac_s_2.png")])
 moving_obj.pac_imgs.append([PhotoImage(file="media/pacs/pac_w_1.png"),
-                            PhotoImage(file="media/pacs/pac_w_2.png"),
-                            PhotoImage(file="media/pacs/pac_w_3.png"),
-                            PhotoImage(file="media/pacs/pac_w_4.png")])
-moving_obj.ghost_imgs = [PhotoImage(file="media/ghosts/ghost_red.png"),
-                             PhotoImage(file="media/ghosts/ghost_green.png"),
-                             PhotoImage(file="media/ghosts/ghost_orange.png"),
-                             PhotoImage(file="media/ghosts/ghost_pink.png"),
-                             PhotoImage(file="media/ghosts/ghost_blue.png"),
-                             PhotoImage(file="media/ghosts/ghost_eyes.png")]
+                            PhotoImage(file="media/pacs/pac_w_2.png")])
+moving_obj.ghost_imgs =[PhotoImage(file="media/ghosts/vilo.png"),
+                            PhotoImage(file="media/ghosts/vene.png"),
+                            PhotoImage(file="media/ghosts/prank.png"),
+                            PhotoImage(file="media/ghosts/niitsoo.png"),
+                            PhotoImage(file="media/ghosts/hein.png"),
+                            PhotoImage(file="media/ghosts/plank.png"),
+                            PhotoImage(file="media/ghosts/tamm.png"),
+                            PhotoImage(file="media/ghosts/nõlv.png"),
+                            PhotoImage(file="media/ghosts/pungas.png")]
+                            # PhotoImage(file="media/ghosts/paales.png"),
+                            # PhotoImage(file="media/ghosts/annamaa.png"),
+                            # PhotoImage(file="media/ghosts/palm.png")]
+textfile = open("media/texts.txt", encoding="utf-8")
+ghostmsg = {}
+for line in textfile:
+    ghostmsg[line.split(": ")[0].lstrip("\ufeff")] = line.split(": ")[1].split("; ")
+textfile.close()
+
 thisgame = game(tahvel)
-thisgame.parent=menu
+thisgame.parent = menu
 thisgame.create_mainmenu()
 
-def nool_üles(event):
+def up_press(event):
     if thisgame.state == "game":
         moving_obj.pac.new_dir = [0,-10]
         moving_obj.pac.new_dir_imgs = moving_obj.pac_imgs[1]
     elif thisgame.state == "menu":
         thisgame.nextselection(-1)
 
-def nool_alla(event):
+def down_press(event):
     if thisgame.state == "game":
         moving_obj.pac.new_dir = [0,10]
         moving_obj.pac.new_dir_imgs = moving_obj.pac_imgs[2]
     elif thisgame.state == "menu":
         thisgame.nextselection(1)
 
-def nool_vasakule(event):
+def left_press(event):
     if thisgame.state == "game":
         moving_obj.pac.new_dir = [-10,0]
         moving_obj.pac.new_dir_imgs = moving_obj.pac_imgs[3]
 
-def nool_paremale(event):
+def right_press(event):
     if thisgame.state == "game":
         moving_obj.pac.new_dir = [10,0]
         moving_obj.pac.new_dir_imgs = moving_obj.pac_imgs[0]
@@ -81,25 +78,48 @@ def spacebar_press(event):
             menu.grid_forget()
             thisgame.parent = tahvel
             tahvel.grid()
+            thisgame.level = 'baka'
             thisgame.create_level()
             thisgame.create_gamemenu()
             thisgame.state = "game"
         elif thisgame.selection == 1:
-            pass
+            if thisgame.highestlevelcompleted != None:
+                menu.grid_forget()
+                thisgame.parent = tahvel
+                tahvel.grid()
+                thisgame.level = 'mag'
+                thisgame.create_level()
+                thisgame.create_gamemenu()
+                thisgame.state = "game"
+            else:
+                thisgame.create_attention_message()
         elif thisgame.selection == 2:
-            pass
+            if thisgame.highestlevelcompleted == "mag":
+                menu.grid_forget()
+                thisgame.parent = tahvel
+                tahvel.grid()
+                thisgame.level = 'dok'
+                thisgame.create_level()
+                thisgame.create_gamemenu()
+                thisgame.state = "game"
+            else:
+                thisgame.create_attention_message()
         elif thisgame.selection == 3:
-            raam.destroy()
+            thisgame.create_help_message()
+        elif thisgame.selection == 4:
+            frame.destroy()
     elif thisgame.state == "game":
         thisgame.state = "pause"
     elif thisgame.state == "pause":
         thisgame.state = "game"
     elif thisgame.state == "over":
-        pass #tagasi algmenüüsse
+        thisgame.state = "menu"
+        thisgame.parent = menu
+        thisgame.create_mainmenu()
 
 def escape_press(event):
     if thisgame.state == "menu":
-        raam.destroy()
+        frame.destroy()
     elif thisgame.state != "menu":
         thisgame.state = "menu"
         for elem in immov_obj.walls:
@@ -121,22 +141,55 @@ def escape_press(event):
         menu.grid()
 
 # seon nooleklahvid vastavate funktsioonidega
-raam.bind_all("<Up>", nool_üles)
-raam.bind_all("<Down>",  nool_alla)
-raam.bind_all("<Left>",  nool_vasakule)
-raam.bind_all("<Right>", nool_paremale)
-raam.bind_all("<space>", spacebar_press)
-raam.bind_all("<Escape>", escape_press)
+frame.bind_all("<Up>", up_press)
+frame.bind_all("<Down>",  down_press)
+frame.bind_all("<Left>",  left_press)
+frame.bind_all("<Right>", right_press)
+frame.bind_all("<space>", spacebar_press)
+frame.bind_all("<Escape>", escape_press)
+frame.bind_all("<Return>", spacebar_press)
+
 
 def uuenda():
     if thisgame.state == "game":
         for elem in moving_obj.ghosts:
-            elem.move()
-        moving_obj.pac.move()
-        thisgame.score.config(text="SCORE:"+str(moving_obj.pac.score))
-        thisgame.lives.config(text="LIVES:"+str(moving_obj.pac.lives))
-    # ootame 0,1 sekundit ja siis uuendame positsiooni
-    raam.after(80, uuenda)
+            elem.move(thisgame)
+        moving_obj.pac.move(thisgame)
+        thisgame.ainepunktid.config(text="EAP: "+str(int(moving_obj.pac.ainepunktid)))
+        thisgame.hoiatused.config(text="HOIATUSI: "+str(moving_obj.pac.hoiatused))
+        if moving_obj.pac.coll_name != None:
+            thisgame.viimanehoiatus.config(text=ghostmsg[moving_obj.pac.coll_name][randint(0,1)])
+            thisgame.turns_to_show_warning = 80
+            moving_obj.pac.coll_name = None
+        if thisgame.turns_to_show_warning:
+            thisgame.turns_to_show_warning -= 1
+        else:
+            thisgame.viimanehoiatus.config(text="")
+        if moving_obj.pac.hoiatused == 3:
+            thisgame.state = "pause"
+            thisgame.create_gameover_message()
+            escape_press("<Escape>")
+        if thisgame.state == "game" and len(immov_obj.pellets) == 0:
+            thisgame.state = "pause"
+            thisgame.create_victory_message()
+            escape_press("<Escape>")
+            if thisgame.level == 'baka':
+                thisgame.highestlevelcompleted = 'baka'
+                thisgame.selection_pointer.place(x=35, y=100+1*40)
+                thisgame.selection = 1
+            elif thisgame.level == 'mag':
+                thisgame.highestlevelcompleted = 'mag'
+                thisgame.selection_pointer.place(x=35, y=100+2*40)
+                thisgame.selection = 2
+            else:
+                thisgame.selection_pointer.place(x=35, y=100+4*40)
+                thisgame.selection = 4
+
+    # ootame 0,06 sekundit ja siis uuendame positsiooni
+    if thisgame.level == "dok":
+        frame.after(60, uuenda)
+    else:
+        frame.after(80, uuenda)
 
 uuenda()
-raam.mainloop()
+frame.mainloop()
--- objects.py
+++ objects.py
@@ -1,4 +1,5 @@
-__author__ = 'Eduard'
+__author__ = 'Meelis Tapo'
+__author__ = 'Eduard Šengals'
 import random
 from game import *
 class moving_obj:
@@ -7,7 +8,7 @@ class moving_obj:
     pac_imgs = []
     ghost_imgs = []
     moving = []
-    def __init__(self, start_pos, parent, img, coll_pel = False, coll_ghost = False, att=None):
+    def __init__(self, start_pos, parent, img, coll_pel = False, coll_ghost = False, att=None, name=None):
         self.coll_pel = coll_pel
         self.coll_ghost = coll_ghost
         self.parent = parent
@@ -22,15 +23,15 @@ class moving_obj:
         self.att = att
         self.new_dir = None
         self.possible_dir = []
-        self.score = 0
-        self.lives = 3
-        self.skipturns = 0
-        self.respawnindex = 0
+        self.ainepunktid = 0.0
+        self.hoiatused = 0
+        self.name = name
+        self.coll_name = None
         if self.att != "player":
             moving_obj.ghosts.append(self)
         else:
             moving_obj.pac = self
-    def move(self):
+    def move(self, game):
         if (not self.pos[0]%25 and not self.pos[1]%25) and (self.pos[0]%50 or self.pos[1]%50):
             self.possible_dir.clear()
             if [self.pos[0]+50,self.pos[1]] not in immov_obj.walls.values():
@@ -41,55 +42,61 @@ class moving_obj:
                 self.possible_dir.append([0,10])
             if [self.pos[0],self.pos[1]-50] not in immov_obj.walls.values():
                 self.possible_dir.append([0,-10])
-            if self.dir != None and self.att != "player" and [-self.dir[0],-self.dir[1]] in self.possible_dir:
-                self.possible_dir.remove([-self.dir[0],-self.dir[1]])
+            if self.att != "player": #><
+                if self.dir != None and [-self.dir[0],-self.dir[1]] in self.possible_dir and len(self.possible_dir) != 1:
+                    self.possible_dir.remove([-self.dir[0],-self.dir[1]])
+                if self.att == "ai":
+                    self.check_lineofsight()
     #----------------------------------------------------------------
         if self.att == "player":
             if self.new_dir in self.possible_dir:
                 self.dir = self.new_dir
                 self.dir_imgs = self.new_dir_imgs
-        elif self.att == "tackle":
-            #Siia tuleb kunagi lõksupüüdja loogika
-            pass
-        elif self.att == "chase":
-            #Siia tuleb kunagi tagaajaja loogika
-            pass
+        elif self.att == "ai":
+            if moving_obj.pac.pos[0] in range(self.lineofsight[0][0], self.lineofsight[0][1])and moving_obj.pac.pos[0] < self.pos[0] and [-10, 0] in self.possible_dir:
+                self.dir = [-10, 0]
+            elif moving_obj.pac.pos[0] in range(self.lineofsight[0][0], self.lineofsight[0][1])and moving_obj.pac.pos[0] > self.pos[0] and [10, 0] in self.possible_dir:
+                self.dir = [10, 0]
+            elif moving_obj.pac.pos[1] in range(self.lineofsight[1][0], self.lineofsight[0][1])and moving_obj.pac.pos[1] < self.pos[1] and [0, -10] in self.possible_dir:
+                self.dir = [0, -10]
+            elif moving_obj.pac.pos[1] in range(self.lineofsight[1][0], self.lineofsight[0][1])and moving_obj.pac.pos[1] > self.pos[1] and [0, 10] in self.possible_dir:
+                self.dir = [0, 10]
+            else:
+                self.dir = self.possible_dir[random.randint(0,len(self.possible_dir)-1)]
         else:
             self.dir = self.possible_dir[random.randint(0,len(self.possible_dir)-1)]
     #----------------------------------------------------------------
-        if not self.skipturns:
-            if self.dir in self.possible_dir:
-                if self.att == "player":
-                    self.parent.move(self.id, self.dir[0], self.dir[1])
-                    self.pos = [self.pos[0]+self.dir[0],self.pos[1]+self.dir[1]]
-                    self.swapimage()
-                    self.possible_dir = [self.dir,[-self.dir[0],-self.dir[1]]]
-                else:
-                    self.parent.move(self.id, self.dir[0], self.dir[1])
-                    self.pos = [self.pos[0]+self.dir[0],self.pos[1]+self.dir[1]]
-                    self.possible_dir = [self.dir]
-        else:
-            self.skipturns -= 1
+        if self.dir in self.possible_dir:
             if self.att == "player":
-                self.respawn()
+                self.parent.move(self.id, self.dir[0], self.dir[1])
+                self.pos = [self.pos[0]+self.dir[0],self.pos[1]+self.dir[1]]
+                self.swapimage()
+                self.possible_dir = [self.dir,[-self.dir[0],-self.dir[1]]]
+            else:
+                self.parent.move(self.id, self.dir[0], self.dir[1])
+                self.pos = [self.pos[0]+self.dir[0],self.pos[1]+self.dir[1]]
+                self.possible_dir = [self.dir]
     #----------------------------------------------------------------
         if self.coll_pel and (self.pos in immov_obj.pellets.values()):
-            self.score += 100
+            if game.level == "baka":
+                self.ainepunktid += 180/130
+            elif game.level == "mag":
+                self.ainepunktid += 120/129
+            elif game.level == "dok":
+                self.ainepunktid += 240/129
             for k, v in immov_obj.pellets.items():
                 if v == self.pos:
                     self.parent.delete(k)
                     break
             del immov_obj.pellets[k]
     #----------------------------------------------------------------
-        if self.coll_ghost and self.skipturns == 0:
+        if self.coll_ghost:
             for elem in moving_obj.ghosts:
                 if abs(self.pos[0]-elem.pos[0])<30 and abs(self.pos[1]-elem.pos[1])<30:
-                    self.skipturns = 0
-                    for ghost in moving_obj.ghosts:
-                        ghost.skipturns = 0
+                    self.coll_name = elem.name
                     self.parent.delete(self.id)
-                    self.lives -= 1
-                    self.id = self.parent.create_image(self.start_pos[0], self.start_pos[1], image=self.dir_imgs[0])
+                    self.hoiatused += 1
+                    self.id = self.parent.create_image(self.start_pos[0], self.start_pos[1], image=self.start_img)
                     self.pos = self.start_pos
                     self.dir, self.new_dir = None, None
                     for ghost in moving_obj.ghosts:
@@ -106,37 +113,42 @@ class moving_obj:
                 self.img_index+=1
         self.parent.delete(self.id)
         self.id = self.parent.create_image(self.pos[0], self.pos[1], image=self.dir_imgs[self.img_index])
-        print("swapped to {}".format(self.dir_imgs[self.img_index]))
-    def respawn(self):
-        if not self.respawnindex:
-            self.img_index = self.respawnindex
-        if self.respawnindex == 9:
-            self.pos = self.start_pos
-        if self.respawnindex < 9:
-            if not self.respawnindex%2:
-                self.parent.delete(self.id)
-            else:
-                self.parent.create_image(self.pos[0], self.pos[1], image = self.dir_imgs[self.img_index])
-                self.img_index += 1
-            self.respawnindex += 1
-        else:
-            if not self.respawnindex%2:
-                self.parent.delete(self.id)
-            else:
-                self.parent.create_image(self.pos[0], self.pos[1], image = self.dir_imgs[self.img_index])
-                self.img_index -= 1
-            self.respawnindex += 1
-            print(self.respawnindex)
+    def check_lineofsight(self):
+        self.lineofsight = [[0,0],[0,0]] # [[xrange][yrange]
+        i = 1
+        while True:
+            if [self.pos[0]-50*i, self.pos[1]] in immov_obj.walls.values():
+                self.lineofsight[0][0] = self.pos[0]-50*i
+                i = 1
+                break
+            i += 1
+        while True:
+            if [self.pos[0]+50*i, self.pos[1]] in immov_obj.walls.values():
+                self.lineofsight[0][1] = self.pos[0]+50*i
+                i = 1
+                break
+            i += 1
+        while True:
+            if [self.pos[0], self.pos[1]-50*i] in immov_obj.walls.values():
+                self.lineofsight[1][0] = self.pos[1]-50*i
+                i = 1
+                break
+            i += 1
+        while True:
+            if [self.pos[0], self.pos[1]+50*i] in immov_obj.walls.values():
+                self.lineofsight[1][1] = self.pos[1]+50*i
+                break
+            i += 1
 
 class immov_obj:
     walls = {}
     pellets = {}
-    def __init__(self, pos, parent, img, is_pellet=False):
+    def __init__(self, pos, parent, img, is_pellet=False, is_wall=False):
         self.pos = pos
         self.parent = parent
         self.img = img
         self.id = self.parent.create_image(self.pos[0], self.pos[1], image=self.img)
         if is_pellet:
             immov_obj.pellets[self.id] = self.pos
-        else:
+        if is_wall:
             immov_obj.walls[self.id] = self.pos
